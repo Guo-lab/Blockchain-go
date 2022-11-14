1e70eecbb2f042839d0b386396d9300698b974e5
Guo-lab <74242889+Guo-lab@users.noreply.github.com>
2022-11-14 22:32:27 +0800

    install Golang

---
+++ test.go
@@ -0,0 +1,12 @@
+// install golang on Mac
+// https://go.dev/doc/install
+
+// go tutorial
+// https://www.runoob.com/go/go-program-structure.html
+package main
+
+import "fmt"
+
+func main() {
+   fmt.Println("Hello, World!")
+}
